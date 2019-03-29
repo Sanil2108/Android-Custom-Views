ca297f9fdf636124b47a623da3186903f971dbf8
Sanil <sanilkhurana7@gmail.com>
2019-03-29 18:04:48 +0530

    Added bubble image view

--- app/src/main/java/sanilk/com/customviews/BubbleImageViewActivity.java
+++ app/src/main/java/sanilk/com/customviews/BubbleImageViewActivity.java
@@ -1,10 +1,16 @@
 package sanilk.com.customviews;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import sanilk.com.customviews.views.bubble_image_view.BubbleImageView;
 
@@ -25,6 +31,35 @@ public class BubbleImageViewActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 bubbleImageView.startAnimation();
+            }
+        });
+
+        SeekBar durationSeekBar = findViewById(R.id.activity_bubble_image_view_duration_seek);
+
+        durationSeekBar.setProgress((int)BubbleImageView.DEFAULT_DURATION);
+
+        if(Build.VERSION.SDK_INT > 25) {
+            durationSeekBar.setMin(100);
+            durationSeekBar.setMax(2000);
+        }
+
+        final Context context = this;
+
+        durationSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                bubbleImageView.setCurrentDuration(progress);
+                Toast.makeText(context, -progress+"", Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/sanilk/com/customviews/views/bubble_image_view/BubbleImageView.java
+++ app/src/main/java/sanilk/com/customviews/views/bubble_image_view/BubbleImageView.java
@@ -5,9 +5,15 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -15,52 +21,92 @@ import sanilk.com.customviews.R;
 
 public class BubbleImageView extends LinearLayout {
 
+    public static final float DEFAULT_FREQUENCY = -7;
+    public static final float DEFAULT_AMPLITUDE = -0.3f;
+    public static final long DEFAULT_DURATION = 600;
+
+    private float currentFrequency;
+    private float currentAmplitude;
+    private long currentDuration;
+
     private Context mContext;
-
-    private ObjectAnimator mIncreaseScaleAnimator;
-    private ObjectAnimator mDecreaseScaleAnimator;
-
-    private final static int SCALE_INCREASE_SPEED = 500;
-    private final static int SCALE_DECREASE_SPEED = 500;
 
     private ImageView mImageView;
 
-    private final static float SCALE_INCREASE_DEFAULT = 1.5f;
-    private final static float SCALE_DECREASE_DEFAULT = 0.8f;
+    private Animation animation;
+    private MyBounceInterpolator myBounceInterpolator;
+
+    public void setCurrentDuration(long currentDuration) {
+        this.currentDuration = currentDuration;
+        animation.setDuration(currentDuration);
+    }
+
+//    public void setCurrentAmplitude(float currentAmplitude) {
+//        this.currentAmplitude = currentAmplitude;
+//        myBounceInterpolator.setAmplitude(currentAmplitude);
+//    }
+//
+//    public void setCurrentFrequency(float currentFrequency) {
+//        this.currentFrequency = currentFrequency;
+//        myBounceInterpolator.setFrequency(currentFrequency);
+//    }
 
     public BubbleImageView(Context context, AttributeSet attributeSet){
         super(context, attributeSet);
 
+
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(
+                attributeSet,
+                R.styleable.BubbleImageView,
+                0, 0
+        );
+        Drawable drawable = getResources().getDrawable(typedArray.getResourceId(R.styleable.BubbleImageView_src, -1));
+
+        // Setting default variables
+        currentAmplitude = DEFAULT_AMPLITUDE;
+        currentDuration = DEFAULT_DURATION;
+        currentFrequency = DEFAULT_FREQUENCY;
+
         this.mContext =context;
 
         mImageView = (ImageView) inflate(context, R.layout.bubble_image_view, null);
+
         this.addView(mImageView);
+        mImageView.setImageDrawable(drawable);
 
-        int rotationFirst = (int)((SCALE_INCREASE_SPEED/((float)SCALE_INCREASE_SPEED + SCALE_DECREASE_SPEED)) * 360);
-        int rotationSecond = 360-rotationFirst;
+        myBounceInterpolator = new MyBounceInterpolator(currentFrequency, currentAmplitude);
 
-        PropertyValuesHolder scaleXProperty = PropertyValuesHolder.ofFloat("scaleX", 1, SCALE_INCREASE_DEFAULT);
-        PropertyValuesHolder scaleYProperty = PropertyValuesHolder.ofFloat("scaleY", 1, SCALE_INCREASE_DEFAULT);
-        PropertyValuesHolder rotationProperty = PropertyValuesHolder.ofFloat("rotation", 0, rotationFirst);
-
-
-        PropertyValuesHolder scaleDownXProperty = PropertyValuesHolder.ofFloat("scaleX", SCALE_INCREASE_DEFAULT, SCALE_DECREASE_DEFAULT);
-        PropertyValuesHolder scaleDownYProperty = PropertyValuesHolder.ofFloat("scaleY", SCALE_INCREASE_DEFAULT, SCALE_DECREASE_DEFAULT);
-        PropertyValuesHolder rotationDownProperty = PropertyValuesHolder.ofFloat("rotation", rotationFirst, 360);
-
-        mIncreaseScaleAnimator = ObjectAnimator.ofPropertyValuesHolder(mImageView, scaleXProperty, scaleYProperty, rotationProperty).setDuration(SCALE_INCREASE_SPEED);
-
+        animation = AnimationUtils.loadAnimation(mContext, R.anim.bounce);
+        animation.setInterpolator(new MyBounceInterpolator(currentFrequency, currentAmplitude));
+        animation.setDuration(currentDuration);
     }
 
     public void startAnimation(){
-        mIncreaseScaleAnimator.start();
-        mIncreaseScaleAnimator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                mDecreaseScaleAnimator.start();
-            }
-        });
+        mImageView.startAnimation(animation);
     }
 
+
+    private static class MyBounceInterpolator implements Interpolator{
+
+        private float frequency;
+        private float amplitude;
+
+        public void setFrequency(float frequency) {
+            this.frequency = frequency;
+        }
+
+        public void setAmplitude(float amplitude) {
+            this.amplitude = amplitude;
+        }
+
+        public MyBounceInterpolator(float frequency, float amplitude) {
+            this.frequency = frequency;
+            this.amplitude = amplitude;
+        }
+
+        @Override
+        public float getInterpolation(float input) {
+            return (float)(-(Math.exp(input/amplitude)*Math.cos(frequency*input))+1);
+        }
+    }
 }
